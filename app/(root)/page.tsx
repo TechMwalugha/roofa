65ec7dda8886df8acdd2abc018b11de34cffb478
TechMwalugha <emmanuelmwalugha001@gmail.com>
2023-11-13 17:14:41 +0300

    home page updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,16 @@
 
 import { CarouselDefault } from "@/components/cards/Carousel";
 import rentals from "@/lib/testData/rentalsData";
-import Link from "next/link";
 
 
 export default function Home() {
 
   return (
    <section className='px-2 flex flex-wrap items-center gap-2 xs:flex-row'>
-      {rentals.map((rental, index) => (
+      {/* {rentals.map((rental, index) => (
         <CarouselDefault key={index} images={rental.src} name={rental.name} />   
-      ))}
+      ))} */}
+      <h1>Hello this is the home page</h1>
     </section>
   )
 }
