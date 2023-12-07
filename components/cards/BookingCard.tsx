7380d890b2b58279102075fb254e5e58fe956a31
TechMwalugha <emmanuelmwalugha001@gmail.com>
2023-12-07 12:58:20 +0300

    updates

--- components/cards/BookingCard.tsx
+++ components/cards/BookingCard.tsx
@@ -1,23 +1,34 @@
 import Link from "next/link"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover"
 
+  
 const BookingCard = ({ availableRooms} : { availableRooms: number}) => {
   return (
-    <div className="fixed bottom-4 right-1 shadow rounded-lg bg-white p-3">
-      {availableRooms >= 1 && (
-        <div className="flex flex-col items-center justify-center h-full">
-          <span className="text-base1-semibold mb-3">House available for booking</span>
-          <span className="text-small-medium mb-3">Available rooms:</span>
-          <span className="text-body1-bold mb-4">{availableRooms}</span>
+    <Popover>
+        <PopoverTrigger 
+        className="fixed bottom-3 right-2 bg-blue p-6 rounded-lg shadow hover:text-white transition-all"
+        >check availability</PopoverTrigger>
+        <PopoverContent className="block">
+            { availableRooms >= 1 && (
+                <div className="">
+                    <p className="text-heading3-bold">Available</p>
+                    <p className="text-small-medium mb-3">Reserve this place today</p>
+                    <Link 
+                    href={""} 
+                    className="text-small-medium block w-full bg-danger p-3 rounded-md text-center">Reserve</Link>
+                </div>
+            )}
 
-          <Link
-          href={`/`}
-          className="bg-blue p-3 w-full text-center rounded text-slate-200"
-          >
-            Reserve
-          </Link>
-        </div>
-      )}
-    </div>
+            { availableRooms < 1 && (
+                <p className="text-small-semibold mb-3">Opps! the rental is full. <br /> check houses near ⬇️</p>
+            )}
+        </PopoverContent>
+    </Popover>
+
   )
 }
 
